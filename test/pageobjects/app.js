8d62bcb852027cea1295e3fa06a1e24dc7aadb6f
Nikita Chuchelov <>
2021-02-22 19:53:38 +0300

    optimisations

--- test/pageobjects/app.js
+++ test/pageobjects/app.js
@@ -1,97 +1,107 @@
 
 class App{
-    get DIRECTION_UP() {return 1}
-    get DIRECTION_DOWN() {return -1}
-    get DATE_BAR() {return 0}
-    get HOUR_BAR() {return 1}
-    get MINUTE_BAR() {return 2}
+  get DIRECTION_UP() {return 1}
+  get DIRECTION_DOWN() {return -1}
+  get DAY_BAR() {return 0}
+  get HOUR_BAR() {return 1}
+  get MINUTE_BAR() {return 2}
 
-    getCurrentDate(){
-      let seekBar = $$('//android.widget.SeekBar')
-      return {day:seekBar[0].getText(), hour:seekBar[1].getText(), minute:seekBar[2].getText()}
-    }
-    
-    openReminderWindow(){
-        let sendButton = $$('//android.view.View[@content-desc="Send"]')[0]
-        let loc = sendButton.getLocation()
-        driver.touchPerform([
-            {
-                action: "press",
-                options: {
-                x: loc.x,
-                y: loc.y
-                }
-            },
-            { action: "wait", options: { mseconds: 1000 } },
-            // {
-            //   action: "moveTo",
-            //   options: {
-            //     x: to.x,
-            //     y: to.y
-            //   }
-            // },
-            { action: "release" }
-            ]);
-    
-            driver.touchPerform([
-            {
-                action: "press",
-                options: {
-                x: loc.x,
-                y: loc.y - 50
-                }
-            },
-            { action: "wait", options: { mseconds: 200 } },
-            // {
-            //   action: "moveTo",
-            //   options: {
-            //     x: to.x,
-            //     y: to.y
-            //   }
-            // },
-            { action: "release" }
-            ]);
-    }
-
-    setMessageText(text){
-        $$('//android.widget.EditText')[0].setValue(text)
-    }
-    /**
-     * Swipes given seekbar for 1 value in given direction
-     * 
-     * @param {int} seekBarType Index of wheel to rotate, 0 1 2 for date, hour, minute
-     * @param {int} direction const for direction of turn
-     */
-    swipeSeekbar(seekIndex, direction){
-        let seekBar = $$('//android.widget.SeekBar')
-        let loc1 = seekBar[seekIndex].getLocation()
-        let but = $$('//android.widget.Button')[0].getLocation()
-        let from = {x:loc1.x, y : loc1.y}
-        let elemHeight = (but.y-loc1.y)/6
-        let to = { x:loc1.x, y: from.y + (elemHeight * direction)} 
-        driver.touchPerform([
+  getCurrentDate(){
+    let seekBar = $$('//android.widget.SeekBar')
+    return {day:seekBar[0].getText(), hour:seekBar[1].getText(), minute:seekBar[2].getText()}
+  }
+  
+  openReminderWindow(){
+      let sendButton = $$('//android.view.View[@content-desc="Send"]')[0]
+      let loc = sendButton.getLocation()
+      driver.touchPerform([
           {
-            action: "press",
-            options: {
-              x: from.x,
-              y: from.y
-            }
+              action: "press",
+              options: {
+              x: loc.x,
+              y: loc.y
+              }
           },
-          { action: "wait", options: { mseconds: 500 } },
-          {
-            action: "moveTo",
-            options: {
-              x: to.x,
-              y: to.y
-            }
-          },
+          { action: "wait", options: { mseconds: 1000 } },
+          // {
+          //   action: "moveTo",
+          //   options: {
+          //     x: to.x,
+          //     y: to.y
+          //   }
+          // },
           { action: "release" }
-        ]);
-      }
+          ]);
+  
+          driver.touchPerform([
+          {
+              action: "press",
+              options: {
+              x: loc.x,
+              y: loc.y - 50
+              }
+          },
+          { action: "wait", options: { mseconds: 200 } },
+          // {
+          //   action: "moveTo",
+          //   options: {
+          //     x: to.x,
+          //     y: to.y
+          //   }
+          // },
+          { action: "release" }
+          ]);
+  }
 
-      setReminder(){
-        $$('//android.widget.Button')[0].click()
-      }
+  setMessageText(text){
+      $$('//android.widget.EditText')[0].setValue(text)
+  }
+  /**
+   * Swipes given seekbar for 1 value in given direction
+   * 
+   * @param {int} seekBarType Index of wheel to rotate, 0 1 2 for date, hour, minute
+   * @param {int} direction const for direction of turn
+   * @param {int} reps optional amount of repetiotions, default = 1
+   */
+  swipeSeekbar(seekIndex, direction, reps = 1){
+    let seekBar = $$('//android.widget.SeekBar')
+    let loc1 = seekBar[seekIndex].getLocation()
+    let but = $$('//android.widget.Button')[0].getLocation()
+    let from = {x:loc1.x, y : loc1.y}
+    let elemHeight = (but.y-loc1.y)/6
+    let to = { x:loc1.x, y: from.y + (elemHeight * direction)} 
+    for (let i = 0; i<reps; i++){
+      driver.touchPerform([
+        {
+          action: "press",
+          options: {
+            x: from.x,
+            y: from.y
+          }
+        },
+        { action: "wait", options: { mseconds: 500 } },
+        {
+          action: "moveTo",
+          options: {
+            x: to.x,
+            y: to.y
+          }
+        },
+        { action: "release" }
+      ]);
     }
+  }
+
+  setReminder(){
+    $$('//android.widget.Button')[0].click()
+  }
+
+  setDateByOffset(offset){
+    this.swipeSeekbar(this.DAY_BAR, DIRECTION_DOWN, offset.days)
+    this.swipeSeekbar(this.HOUR_BAR, DIRECTION_DOWN, offset.hours)
+    this.swipeSeekbar(this.MINUTE_BAR, DIRECTION_DOWN, offset.minutes)
+  }
+  
+}
     
     module.exports = new App()
--- test/specs/boost.spec.js
+++ test/specs/boost.spec.js
@@ -3,25 +3,12 @@ const App = require('../pageObjects/app')
 
 describe('LVL UP MY JABA', ()=>{
 
-    //telegram
-    it('SWEEP THE SWIPES', ()=>{
+  //offset structure {days : x, hours : y, minutes: z}, x,y,z - int
 
-      App.setMessageText('BOOSTING IS NOT CHEATING')
-      App.openReminderWindow()
-
-      //set date setup sequence
-      App.swipeSeekbar(App.DATE_BAR, App.DIRECTION_DOWN)
-      App.swipeSeekbar(App.DATE_BAR, App.DIRECTION_DOWN)
-      App.swipeSeekbar(App.HOUR_BAR, App.DIRECTION_DOWN)
-      App.swipeSeekbar(App.MINUTE_BAR, App.DIRECTION_DOWN)
-      App.swipeSeekbar(App.MINUTE_BAR, App.DIRECTION_DOWN)
-      App.swipeSeekbar(App.MINUTE_BAR, App.DIRECTION_DOWN)
-
-      //check whats set up
-      console.log(App.getCurrentDate())
-
-      App.setReminder()
-        
-    })
-
+  it('SWEEP THE SWIPES', ()=>{
+    App.setMessageText('BOOSTING IS NOT CHEATING')
+    App.openReminderWindow()
+    App.setDateByOffset({days:0,hours:2,minutes:10})
+    App.setReminder()
+  })
 })
